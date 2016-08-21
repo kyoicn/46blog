1ecca60462aef6a8dcb96b3b5734a02d951920db
kyoi <kyoi.cn@gmail.com>
2016-08-21 00:50:42 -0700

    add bot and dbsaver

---
+++ bot.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+from __future__ import print_function
+from ConfigParser import ConfigParser
+from dbsaver import DBSaver
+from feedfetcher import FeedFetcher
+from twitterbot import TwitterBot
+import argparse
+import sys
+import time
+
+argparser = argparse.ArgumentParser(
+    prog = '46blog bot',
+    description = 'Options for fetching, parsing, saving and tweeting data.',
+    epilog = 'うちらは乃木坂登り坂！')
+argparser.add_argument(
+    '-v', '--verbose',
+    action = 'count',
+    default = 0,
+    help = 'verbose output, use multiple times to indicate verbosity level')
+argparser.add_argument(
+    '-f', '--no_cache',
+    action = 'store_true',
+    help = 'disable caching, all fetches are considered as new')
+argparser.add_argument(
+    '-t', '--twitter',
+    action = 'store_true',
+    help = 'enable to tweet new posts')
+argparser.add_argument(
+    '-d', '--database',
+    action = 'store_true',
+    help = 'enable to store in database')
+argparser.add_argument(
+    '-b', '--interval',
+    action = 'store',
+    type = int,
+    default = 60,
+    help = 'interval in seconds between two rounds of fetches, default 60s')
+argparser.add_argument(
+    '-c', '--config_file',
+    action = 'store',
+    type = str,
+    default = 'config.ini',
+    help = 'specify config file, default is config.ini')
+
+args = argparser.parse_args()
+if args.verbose > 0:
+    print(vars(args))
+
+cp = ConfigParser()
+cp.read(args.config_file)
+
+fetcher = FeedFetcher(args.config_file)
+
+if args.twitter:
+    twitter_bot = TwitterBot(args.config_file)
+    tweeted_file = open(cp.get('General', 'tweeted'), 'r+')
+    tweeted = set(tweeted_file.read().split('\n'))
+
+if args.database:
+    db_saver = DBSaver()
+
+while True:
+    try:
+        for entry in reversed(fetcher.fetch()):
+            # Main loop
+
+            # DB saver
+            # TODO: async
+            if args.database:
+                print('save to db')
+
+            # Twitter bot
+            # TODO: async
+            if args.twitter:
+                if entry.hashcode() not in tweeted:
+                    twitter_bot.tweet(entry)
+                    tweeted.add(entry.hashcode())
+                    tweeted_file.write('{}\n'.format(entry.hashcode()))
+                    print('tweeted: {0}/{1}'.format(
+                        entry.get_author(),
+                        entry.get_title()))
+
+        time.sleep(args.interval)
+
+    except KeyboardInterrupt:
+        if args.verbose > 0:
+            print('Keyboard Interrupt: clean up')
+        if args.twitter:
+            if args.verbose > 0:
+                print('...closing tweeted_file', end = '')
+            tweeted_file.close()
+            if args.verbose > 0:
+                print('[OK]')
+        if args.database:
+            if args.verbose > 0:
+                print('...closing db connection', end='')
+            # close connection
+            if args.verbose > 0:
+                print('[OK]')
+
+        sys.exit('The bot is terminated by you.')
+
+    except Exception as e:
+        print(str(e))
---
+++ dbsaver.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+from sql import *
+import MySQLdb
+
+class DBSaver:
+    """Talks to DB"""
+
+    def __init__(self):
+        # TODO: load from config
+        self._db = MySQLdb.connect(host = 'localhost',
+                                  user = 'root',
+                                  passwd = 'test',
+                                  db = 'nogizaka_blog')
+        self._cur = self._db.cursor()
+        self._entry_table = Table('entry')
+        self._member_table = Table('member')
+        self._image_table = Table('image')
+        # TODO: load necessary db data, e.g. member names
+
+    """Save entry data to database"""
+    def save(self, entry):
+        # try:
+            # TODO: log.atfine()
+            print '[{}]'.format(datetime.now()) + 'start saving to db:'
+
+            insert = self._insert_entry_sql(entry)
+            #self._cur.execute(insert[0], insert[1])
+
+            insert = self._insert_image_sql(entry)
+            print insert
+            self._cur.executemany(insert[0], insert[1])
+
+            return True
+        # except Exception as e:
+        #     # TODO: log
+        #     print str(e)
+        #     return False
+
+    def _insert_entry_sql(self, entry):
+        if entry.is_readonly():
+            raise Exception(
+                """
+                This entry is read-only, storing operations are not allowed.
+                """)
+        # TODO: load columns from config, and use map() here
+        columns = [
+            self._entry_table.id,
+            self._entry_table.publish_time,
+            self._entry_table.author_id,
+            self._entry_table.author_name,
+            self._entry_table.title,
+            self._entry_table.text,
+            self._entry_table.permalink,
+            self._entry_table.raw_html,
+        ]
+        values = [[
+            entry.hashcode(),
+            entry.get_time_str(),
+            #self._name_id_map[entry.get_author()],
+            1,
+            entry.get_author(),
+            entry.get_title(),
+            entry.get_text(),
+            entry.get_permalink(),
+            entry.get_html()
+        ]]
+        return tuple(self._entry_table.insert(columns = columns,
+                                              values = values))
+
+    def _insert_image_sql(self, entry):
+        if entry.is_readonly():
+            raise Exception(
+                """
+                This entry is read-only, storing operations are not allowed.
+                """)
+        # TODO: load columns from config, and use map() here
+        columns = [
+            self._image_table.id,
+            self._image_table.entry_id,
+            self._image_table.remote_url,
+            self._image_table.remote_url_2,
+            self._image_table.local_url,
+            self._image_table.content,
+            self._image_table.extension
+        ]
+        values = []
+        for image in entry.get_images():
+            values.append([
+                image.hashcode(),
+                entry.hashcode(),
+                image.get_remote_url(),
+                image.get_remote_url(False),
+                image.get_local_url(),
+                # image.get_content(),
+                '',
+                image.get_extension()
+            ])
+        return tuple(self._image_table.insert(columns = columns,
+                                              values = values))
